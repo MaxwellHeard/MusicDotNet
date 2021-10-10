59b101300a77148e813047071a8f08df1b0cdfe6
MaxwellHeard <77406315+MaxwellHeard@users.noreply.github.com>
2021-10-09 23:31:36 -0400

    Added albums controller and model

---
+++ Controllers/AlbumsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicDotNet.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAlbum(string album)
+        {
+            //if the album is empty redirect to index
+            if (album == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.album = album;
+            return View();
+        }
+    }
+}
